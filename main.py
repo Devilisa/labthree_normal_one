974c7cee54e8ab0664aa30025f49d6682796d322
Devilisa <betskovase@bk.ru>
2020-12-22 16:27:28 +0300

    Исправила

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import math
 import struct
+import numpy as np
 
 
-File_Width = 60
-File_Height = 60
+File_Width = 100
+File_Height = 100
 File_Size = 0
 Color_depth = 4
 Color_num = 32
 File_Offset = 54
-maxt = 10*math.pi
+maxt = 10*np.pi
 Max_range = File_Width // 2 + 1
 Min_range = -((File_Width // 2) - 1)
 
 points = []
 # x^2 + y^2 = 631,04 + 198,4 * cos(7,2t) отсюда выразим y через x
-for t in range(0, round(maxt + 1)):
+for t in np.range(0, maxt + 0.1, 0.001):
     x = round(24.8 * (math.cos(t) + math.cos(6.2 * t) / 6.2))
     y = round(((631.04 + 198.4 * math.cos(7.2*t) - x ** 2)**0.5).real)
     if [x, y] not in points:
@@ -29,8 +30,8 @@ with open("graph.bmp", "wb") as fail:
     # создали блок BITMAPINFOHEADER
     for y in range(Min_range, Max_range):
         for x in range(Min_range, Max_range):
-            coords = [x, y]
-            if coords in points:
+            cords = [x, y]
+            if cords in points:
                 fail.write(struct.pack("<i", 0))
             else:
                 fail.write(struct.pack("<i", 16777215))
